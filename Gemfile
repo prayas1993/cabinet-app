46b20f7685565eb7d2e51cd9577060f5fd058436
HEY <you@example.com>
2017-05-08 16:51:41 +0530

    Add docs views and define actions

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-gem 'simple_form', '~> 3.4'
+gem 'simple_form', '~> 3.4' 
 gem 'devise', '~> 4.2', '>= 4.2.1'     
 gem 'haml', '~> 5.0', '>= 5.0.1'
 # Use ActiveModel has_secure_password
--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,16 +1,27 @@
 class DocsController < ApplicationController
+ before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
 def index
+	@docs=Doc.all.order("created_at DESC")
 end
 
 def show
 end
 
 def new
+	@doc=Doc.new
 end
 
 def create
-end
+	@doc=Doc.new(doc_params)
+       
+    if @doc.save
+	  redirect_to @doc
+	else
+      render 'new'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    end
+ end
+
 
 def edit
 end 	
@@ -24,9 +35,11 @@ end
   private
 
     def find_doc
+    	@doc=Doc.find(params[:id])
     end
 
     def doc_params
+    	params.require(:doc).permit(:title,:content)
     end
 
 
